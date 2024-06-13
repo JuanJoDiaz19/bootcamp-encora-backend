8c4131c07a38268adf5ca42086e1b8db372215a8
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-06-12 21:30:48 -0500

    feat: adding the resources ces of all the modules of the backend

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+import { OrdersModule } from './orders/orders.module';
+import { ShoppingCartModule } from './shopping_cart/shopping_cart.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, CommonModule, OrdersModule, ShoppingCartModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
