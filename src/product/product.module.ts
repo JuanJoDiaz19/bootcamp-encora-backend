b918851d05e4950049d02a8199e14c113bd06c64
Samhernesp <samhernesp@gmail.com>
2024-06-13 16:39:41 -0500

    Add import of entities for typeOrm

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,9 +4,11 @@ import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { Category } from './entities/category.entity';
+import { Review } from './entities/review.entity';
+import { Stock } from './entities/stock.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product,Category])],
+  imports: [TypeOrmModule.forFeature([Product,Category,Review,Stock])],
   controllers: [ProductController],
   providers: [ProductService],
 })
--- src/shopping_cart/shopping_cart.module.ts
+++ src/shopping_cart/shopping_cart.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ShoppingCartService } from './shopping_cart.service';
 import { ShoppingCartController } from './shopping_cart.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ShoppingCart } from './entities/shopping_cart.entity';
+import { CartItem } from './entities/shopping_cart_item.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ShoppingCart, CartItem])],
   controllers: [ShoppingCartController],
   providers: [ShoppingCartService],
 })
