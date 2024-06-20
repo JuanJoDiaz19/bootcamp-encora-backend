abeefcbc8738447a425ac44f83b21150d8e18c17
Samhernesp <samhernesp@gmail.com>
2024-06-19 20:13:56 -0500

    Fix some errors

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -7,10 +7,13 @@ import { Category } from './entities/category.entity';
 import { Review } from './entities/review.entity';
 import { Stock } from './entities/stock.entity';
 import { Group } from './entities/group.entity';
+import { CategoryController } from './category.controller';
+import { GroupController } from './group.controller';
+import { ReviewController } from './review.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product,Category,Review,Stock, Group])],
-  controllers: [ProductController],
+  controllers: [ProductController, CategoryController, GroupController, ReviewController],
   providers: [ProductService],
 })
 export class ProductModule {}
--- src/product/review.controller.ts
+++ src/product/review.controller.ts
@@ -6,7 +6,7 @@ import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
 
 @Controller('review')
-export class ProductController {
+export class ReviewController {
   constructor(private readonly productService: ProductService) {}
 
   @Post('')
