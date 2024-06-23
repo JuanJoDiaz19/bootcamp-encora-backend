8652c04d5173448dac08d235f9650a28973879c7
Skydoes10 <ricardoms1909@gmail.com>
2024-06-23 15:16:20 -0500

    feat: add getReviewsByProductId and findOneByUser in review and shopping cart controllers

--- src/product/controllers/review.controller.ts
+++ src/product/controllers/review.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
 import { ProductService } from '../services/product.service';
 import { DeleteResult } from 'typeorm';
 import { Review } from '../entities/review.entity';
@@ -11,42 +20,72 @@ export class ReviewController {
   constructor(private readonly productService: ProductService) {}
 
   @Post('')
-  createReview(@Body() createReviewDto: CreateReviewDto): Promise<Review>{
+  createReview(@Body() createReviewDto: CreateReviewDto): Promise<Review> {
     return this.productService.createReview(createReviewDto);
   }
 
   @Get('')
-  getAllReviews(@Query('page') page: string, @Query('limit') limit: string): Promise<[Review[],number]>{
-    return this.productService.getAllReviews(page,limit);
+  getAllReviews(
+    @Query('page') page: string,
+    @Query('limit') limit: string,
+  ): Promise<[Review[], number]> {
+    return this.productService.getAllReviews(page, limit);
   }
 
   @Get('info')
-  async getAllReviewsInfo(@Query('page') page: string, @Query('limit') limit: string): Promise<[InfoReviewDto[], number]> {
-    const [reviews, total] = await this.productService.getAllReviews(page, limit);
-    const infoReviews: InfoReviewDto[] = reviews.map(review => new InfoReviewDto(review));
+  async getAllReviewsInfo(
+    @Query('page') page: string,
+    @Query('limit') limit: string,
+  ): Promise<[InfoReviewDto[], number]> {
+    const [reviews, total] = await this.productService.getAllReviews(
+      page,
+      limit,
+    );
+    const infoReviews: InfoReviewDto[] = reviews.map(
+      (review) => new InfoReviewDto(review),
+    );
     return [infoReviews, total];
   }
 
   @Get(':id')
-  getReviewById(@Param('id') id: string): Promise<Review>{
+  getReviewById(@Param('id') id: string): Promise<Review> {
     return this.productService.getReviewById(id);
   }
 
   @Get('info/:id')
-  async getInfoReviewById(@Param('id') id: string): Promise<InfoReviewDto>{
+  async getInfoReviewById(@Param('id') id: string): Promise<InfoReviewDto> {
     const review = await this.productService.getReviewById(id);
     const infoReview = new InfoReviewDto(review);
     return infoReview;
   }
 
+  @Get('product/:id')
+  async getReviewsByProductId(
+    @Param('id') id: string,
+    @Query('page') page: string,
+    @Query('limit') limit: string,
+  ): Promise<[InfoReviewDto[], number]> {
+    const [reviews, total] = await this.productService.getReviewsByProductId(
+      id,
+      page,
+      limit,
+    );
+    const infoReviews: InfoReviewDto[] = reviews.map(
+      (review) => new InfoReviewDto(review),
+    );
+    return [infoReviews, total];
+  }
+
   @Patch(':id')
-  updateReview(@Param('id') id: string, updateReviewDto: UpdateReviewDto): Promise<Review>{
+  updateReview(
+    @Param('id') id: string,
+    updateReviewDto: UpdateReviewDto,
+  ): Promise<Review> {
     return this.productService.updateReview(id, updateReviewDto);
   }
 
   @Delete(':id')
-  deleteReview(@Param('id') id: string): Promise<DeleteResult>{
+  deleteReview(@Param('id') id: string): Promise<DeleteResult> {
     return this.productService.deleteReview(id);
   }
-
 }
--- src/shopping_cart/shopping_cart.service.ts
+++ src/shopping_cart/shopping_cart.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateShoppingCartDto } from './dto/create-shopping_cart.dto';
 import { UpdateShoppingCartDto } from './dto/update-shopping_cart.dto';
 import { ShoppingCart } from './entities/shopping_cart.entity';
@@ -10,19 +14,18 @@ import { PaymentService } from './payment.service';
 
 @Injectable()
 export class ShoppingCartService {
-  
-  constructor(@InjectRepository(ShoppingCart)
-  private readonly shoppingCartRepository: Repository<ShoppingCart>,
-  @InjectRepository(ShoppingCartItem)
-  private readonly shoppingCartItemRepository: Repository<ShoppingCartItem>,
-  private readonly productService: ProductService,
-  private readonly paymentService: PaymentService,
-  ){}
-  
-  
+  constructor(
+    @InjectRepository(ShoppingCart)
+    private readonly shoppingCartRepository: Repository<ShoppingCart>,
+    @InjectRepository(ShoppingCartItem)
+    private readonly shoppingCartItemRepository: Repository<ShoppingCartItem>,
+    private readonly productService: ProductService,
+    private readonly paymentService: PaymentService,
+  ) {}
+
   async createShoppingCart(): Promise<ShoppingCart> {
     const shoppingCart = this.shoppingCartRepository.create();
-    shoppingCart.sub_total=0;
+    shoppingCart.sub_total = 0;
     //const shoppingCart = this.shoppingCartRepository.create({ user: { id: userId } });
     return await this.shoppingCartRepository.save(shoppingCart);
   }
@@ -30,7 +33,6 @@ export class ShoppingCartService {
   async findAll(): Promise<ShoppingCart[]> {
     return this.shoppingCartRepository.find();
   }
-
 
   async findOne(id: string): Promise<ShoppingCart> {
     const shoppingCart = await this.shoppingCartRepository.findOne({
@@ -43,10 +45,25 @@ export class ShoppingCartService {
     }
 
     return shoppingCart;
-  } 
-  
-  
-  async update(shoppingCartId: string, updateShoppingCartDto: UpdateShoppingCartDto): Promise<ShoppingCart> {
+  }
+
+  async findOneByUser(userId: string): Promise<ShoppingCart> {
+    const shoppingCart = await this.shoppingCartRepository.findOne({
+      where: { user: { id: userId } },
+      relations: ['items', 'items.product'],
+    });
+
+    if (!shoppingCart) {
+      throw new NotFoundException('Shopping Cart not found');
+    }
+
+    return shoppingCart;
+  }
+
+  async update(
+    shoppingCartId: string,
+    updateShoppingCartDto: UpdateShoppingCartDto,
+  ): Promise<ShoppingCart> {
     const { productIds, operation } = updateShoppingCartDto;
 
     const shoppingCart = await this.shoppingCartRepository.findOne({
@@ -55,12 +72,14 @@ export class ShoppingCartService {
     });
 
     if (!shoppingCart) {
-      throw new NotFoundException(`Shopping cart with ID: ${shoppingCartId} not found`);
+      throw new NotFoundException(
+        `Shopping cart with ID: ${shoppingCartId} not found`,
+      );
     }
 
     const products = await this.productService.getProductsByIds(productIds);
 
-    console.log(products, "cacac")
+    console.log(products, 'cacac');
 
     if (products.length !== productIds.length) {
       throw new NotFoundException('One or more products not found');
@@ -68,32 +87,40 @@ export class ShoppingCartService {
 
     if (operation === 'add') {
       for (const product of products) {
-        const existingItem = shoppingCart.items.find(item => item.product.id === product.id);
+        const existingItem = shoppingCart.items.find(
+          (item) => item.product.id === product.id,
+        );
         if (existingItem) {
           existingItem.quantity += 1;
         } else {
           const newItem = new ShoppingCartItem();
           newItem.shopping_cart = shoppingCart;
           newItem.product = product;
-          newItem.quantity = 1; 
+          newItem.quantity = 1;
           newItem.price = product.price;
           await this.shoppingCartItemRepository.save(newItem);
           shoppingCart.items.push(newItem);
         }
       }
     } else if (operation === 'remove') {
-      const itemsToRemove = shoppingCart.items.filter(item => productIds.includes(item.product.id));
-      shoppingCart.items = shoppingCart.items.filter(item => !productIds.includes(item.product.id));
+      const itemsToRemove = shoppingCart.items.filter((item) =>
+        productIds.includes(item.product.id),
+      );
+      shoppingCart.items = shoppingCart.items.filter(
+        (item) => !productIds.includes(item.product.id),
+      );
       await this.shoppingCartItemRepository.remove(itemsToRemove);
     } else {
       throw new Error('Invalid operation');
     }
 
-    shoppingCart.sub_total = shoppingCart.items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+    shoppingCart.sub_total = shoppingCart.items.reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0,
+    );
 
     return this.shoppingCartRepository.save(shoppingCart);
-}
-
+  }
 
   async remove(id: string): Promise<void> {
     try {
@@ -117,7 +144,7 @@ export class ShoppingCartService {
       throw new InternalServerErrorException('Error emptying shopping cart');
     }
   }
-  async buy(shoppingCartId: string){
+  async buy(shoppingCartId: string) {
     const shoppingCart = await this.shoppingCartRepository.findOne({
       where: { id: shoppingCartId },
       relations: ['items', 'items.product'],
@@ -126,4 +153,3 @@ export class ShoppingCartService {
     //const response = this.paymentService.generatePaymentLink(shoppingCart.sub_total,order.id,shoppingCart.user.email)
   }
 }
-
