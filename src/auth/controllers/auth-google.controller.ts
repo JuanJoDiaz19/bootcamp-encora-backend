9623430b9d729019484aa4e09d8383f4a4099198
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-06-28 00:54:18 -0500

    feat: adding the oauth2 fixes

--- src/auth/controllers/auth-google.controller.ts
+++ src/auth/controllers/auth-google.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UserService } from '../services/user.service';
 import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
@@ -20,7 +20,13 @@ export class AuthGoogleController {
   @UseGuards(AuthGuard('google'))
   @ApiOperation({ summary: 'Google redirect' })
   @ApiResponse({ status: 200, description: 'Handles Google OAuth redirect and processes the user' })
-  handleRedirect(@Req() req) {
-    return this.authService.handleUserOauth(req.user);
+  async handleRedirect(@Req() req, @Res() res) {
+    try {
+        const user = await this.authService.handleUserOauth(req.user);
+        const front_url = process.env.FRONT_URL || "http://localhost:4000"
+        res.redirect( front_url +`/oauth?token=${user.token}`);
+    } catch(error) { 
+        console.log(error);
+    }
   }
 }
