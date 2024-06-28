2a609baacad5edd1c9058eb0bbec6a60ef25df49
JuanJoDiaz19 <juanjodp1903@gmail.com>
2024-06-28 02:40:49 -0500

    hotfix: cors problem

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:4000',
+    origin: process.env.FRONT_URL || '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, 
   });
